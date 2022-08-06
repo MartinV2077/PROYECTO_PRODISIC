e6b0d036672694ebeb5b1239e913a7469170f892
MartinVZ <martinvz2077@gmail.com>
2022-08-06 12:40:16 -0500

    inicio Frm pedidos

---
+++ PRODISIC.Presentacion/Procesos/Frm_Registro_Pedidos.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace PRODISIC.Presentacion.Procesos
+{
+    partial class Frm_Registro_Pedidos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Pnl_titulo_form = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Pnl_titulo_form.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Pnl_titulo_form
+            // 
+            this.Pnl_titulo_form.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.Pnl_titulo_form.Controls.Add(this.label3);
+            this.Pnl_titulo_form.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Pnl_titulo_form.Location = new System.Drawing.Point(0, 0);
+            this.Pnl_titulo_form.Name = "Pnl_titulo_form";
+            this.Pnl_titulo_form.Size = new System.Drawing.Size(1089, 36);
+            this.Pnl_titulo_form.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(28, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(218, 22);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "REGISTRAR PEDIDOS";
+            // 
+            // Frm_Registro_Pedidos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 600);
+            this.Controls.Add(this.Pnl_titulo_form);
+            this.Name = "Frm_Registro_Pedidos";
+            this.Text = "Frm_Registro_Pedidos";
+            this.Pnl_titulo_form.ResumeLayout(false);
+            this.Pnl_titulo_form.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Pnl_titulo_form;
+        private System.Windows.Forms.Label label3;
+    }
+}
